ca80dadf1ccafbc700ed9e90234072100f519ed4
hadhemi12345 <86959214+hadhemi12345@users.noreply.github.com>
2023-10-11 18:04:43 +0100

    fix try

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,13 +1,14 @@
 /*package com.esprit.examen.services;
 
 import static org.junit.Assert.*;
-import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,12 +18,12 @@ public class StockServiceImplTest {
 	
 	@Test
 	public void testAddStock() {
-	//	List<Stock> stocks = stockService.retrieveAllStocks();
-	//	int expected=stocks.size();
+		List<Stock> stocks = stockService.retrieveAllStocks();
+		int expected=stocks.size();
 		Stock s = new Stock("stock test",10,100);
 		Stock savedStock= stockService.addStock(s);
 		
-	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
+		assertEquals(expected+1, stockService.retrieveAllStocks().size());
 		assertNotNull(savedStock.getLibelleStock());
 		stockService.deleteStock(savedStock.getIdStock());
 		
